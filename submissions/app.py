26d3e9231e221f49f44e84e909ee94cb455b437b
Srishti Guleria <61888364+sg7801@users.noreply.github.com>
2021-11-26 06:20:47 +0530

    Create app.py

---
+++ submissions/app.py
@@ -0,0 +1,32 @@
+from sqli.middlewares import session_middleware, error_middleware, csrf_middleware
+from trafaret_config import commandline
+from argparse import ArgumentParser
+from aiohttp.web import Application
+from sqli.schema.config import CONFIG_SCHEMA
+from sqli.services.db import setup_database
+from .routes import setup_routes
+from sqli.services.redis import setup_redis
+from sqli.utils.jinja2 import csrf_processor, auth_user_processor
+from aiohttp_jinja2 import setup as setup_jinja
+from jinja2.loaders import PackageLoader
+def init(argv):
+    ap = ArgumentParser()
+    commandline.standard_argparse_options(ap, default_config='./config/dev.yaml')
+    options = ap.parse_args(argv)
+    config = commandline.config_from_options(options, CONFIG_SCHEMA)
+    app = Application(
+        debug=True,
+        middlewares=[
+            session_middleware,
+            csrf_middleware,
+            error_middleware,
+        ]
+    )
+    app['config'] = config
+    setup_jinja(app, loader=PackageLoader('sqli', 'templates'),
+                context_processors=[csrf_processor, auth_user_processor],
+                autoescape=True)
+    setup_database(app)
+    setup_redis(app)
+    setup_routes(app)
+    return app
